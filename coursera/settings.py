47a9cf5aa2409052f0e39cedd2fc18c31e29afd9
Sevim Çokoğlu <41289270+sscok@users.noreply.github.com>
2022-10-29 17:59:13 +0300

    Update settings.py

--- coursera/settings.py
+++ coursera/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%#fai68o%@4yyv+x$345+2pwx3#hxa1hmkel5wu!563fn)(9^o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sscok.pythonanywhere.com']
 
 
 # Application definition
